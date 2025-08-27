196d833a5fcdc3b5148e11b29c29720157e98a88
nishchay8571beaift25-ui <nishchay8571.beaift25@chitkara.edu.in>
2025-08-27 10:21:01 +0530

    calculator.py

--- operator hommework/problem1.py
+++ operator hommework/problem1.py
@@ -1,5 +1,6 @@
-num1=int(input("enter the number : "))
-num2=int(input("enter the number : "))
+num1=float(input("enter the number : "))
+num2=float(input("enter the number : "))
+calculation=input("enter sign :")
 
 sum=(num1+num2)
 
@@ -7,9 +8,15 @@ diff=(num1-num2)
 
 product=(num1*num2)
 
-diffrence=(num1/num2)
+divide=(num1/num2)
 
-print("sum of numbers is ",sum)
-print("diff of number is ",diff)
-print("product of number is ", product)
-print("diffrence if number is ",diffrence)
+if calculation == "+":
+    print("sum of numbers is ",sum)
+elif(calculation=="-"):
+    print("diff of number is ",diff)
+elif(calculation=="*"):
+    print("product of number is ", product)
+elif(calculation=="/"):
+    print("diffrence if number is ",divide)
+else:
+    print("enter a calculation sign")
