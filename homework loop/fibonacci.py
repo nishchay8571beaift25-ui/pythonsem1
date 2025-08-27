bf615a5357e2d949c373d39efb41067a0d739164
nishchay8571beaift25-ui <nishchay8571.beaift25@chitkara.edu.in>
2025-08-27 10:53:12 +0530

    Delete homework loop directory

--- homework loop/fibonacci.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter number your terms : "))
-
-a=0
-b=1
-print(" fabonacci series : ")
-for i in range(n):
-    print(a)
-    a,b=b,a+b
--- homework loop/problem 4.py
+++
@@ -1,2 +0,0 @@
-for i in range(1,11):
-    print(f"the square if {i} is {i*i}")
